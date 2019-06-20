75f608dd76d7d3bef6e37edf2e2748c3dd9add08
ConorWilliams <conorwilliams@outlook.com>
2019-06-20 17:24:43 +0100

    atom

--- setup.sh
+++ setup.sh
@@ -11,8 +11,11 @@ echo 'Setting up'
 
 HERE = ~/.dotfiles
 
+apt-get update
+apt-get upgrade
+
 # install a bunch of stuff
-apt-get install i3 ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg scrot xorg xserver-xorg wget unzip wpasupplicant fonts-hack-ttf zsh lxterminal rofi ranger i3lock redshift amixer alsamixer i3blocks xbacklight cmus thunderbird blueman x11-xserver-utils arandr
+apt-get install i3 ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg scrot xorg xserver-xorg wget unzip wpasupplicant fonts-hack-ttf zsh lxterminal rofi ranger i3lock redshift amixer alsamixer i3blocks xbacklight cmus thunderbird blueman x11-xserver-utils arandr pulseaudio acpi atom
 
 
 # set up zsh
@@ -24,13 +27,20 @@ sudo chsh -s $(which zsh)
 
 ln $HERE/zsh/.zpofile ~/.zpofile
 ln $HERE/zsh/.zshrc ~/.zshrc
+ln $HERE/X/.xinitrc ~/.xinitrc
 
 # set up i3
 mkdir ~/.i3/
 ln ~/.i3/config $HERE/i3/config
 ln ~/.imwheelrc $HERE/imwheelrc/.imwheelrc
 
+# install chrome
 mkdir ~/downloads
 wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O ~/downloads/chrome.deb
 sudo dpkg -i ~/downloads/chrome.deb
 sudo apt install -f
+
+apt-get update
+apt-get upgrade
+
+
