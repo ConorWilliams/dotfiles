963f9afd63b37b77cde1d1c8d200371a6d663e32
ConorWilliams <conorwilliams@outlook.com>
2019-06-21 18:44:30 +0100

    lubuntu

--- setup.sh
+++ setup.sh
@@ -14,14 +14,15 @@ mkdir ~/.config
 apt-get update
 apt-get upgrade
 
+# # install a bunch of stuff
+# apt install i3 ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg scrot xorg xserver-xorg wget unzip wpasupplicant fonts-hack-ttf zsh lxterminal rofi ranger i3lock redshift-gtk alsa-utils alsa-base alsa-tools i3blocks xbacklight cmus thunderbird blueman x11-xserver-utils arandr pulseaudio acpi ranger pavucontrol vim unicode python-pip python3-pip zathura software-properties-common golang-go pcmanfm lxtask
+
+add-apt-repository ppa:mmstick76/alacritty
+
 # install a bunch of stuff
-apt install i3 ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg scrot xorg xserver-xorg wget unzip wpasupplicant fonts-hack-ttf zsh lxterminal rofi ranger i3lock redshift-gtk alsa-utils alsa-base alsa-tools i3blocks xbacklight cmus thunderbird blueman x11-xserver-utils arandr pulseaudio acpi ranger pavucontrol vim unicode python-pip python3-pip zathura software-properties-common golang-go pcmanfm lxtask lightdm
+apt install i3 scrot wget fonts-hack-ttf zsh rofi ranger i3lock redshift-gtk alsa-utils alsa-base alsa-tools i3blocks xbacklight cmus thunderbird x11-xserver-utils arandr pulseaudio acpi pavucontrol vim python-pip python3-pip zathura golang-go pcmanfm lxtask alacritty
 
 #texlive-full
-
-# alacritty
-add-apt-repository ppa:mmstick76/alacritty
-apt install alacritty
 
 mkdir ~/.config/alacritty
 ln -s ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
